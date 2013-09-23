4a26d0404a70d268a25f64f3583fffad3be0a921
Xeada <xeada77@gmail.com>
2013-09-23 23:50:30 +0200

    Pagina About creada

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+   describe "Help page" do
+      
+      it "deberia de tener el contenido 'Help'" do
+	visit '/static_pages/help'
+	expect(page).to have_content('Help')
+      end
+   end
+   describe "About page" do
+      it "deberia de tener el contenido 'About Us'" do
+	visit '/static_pages/about'
+	expect(page).to have_content('About Us')
+      end
+   end
+end
