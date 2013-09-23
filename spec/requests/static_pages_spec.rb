1451b27d2043bfa4fa4b30096d42eba0b97114c9
Xeada <xeada77@gmail.com>
2013-09-24 00:37:14 +0200

    Final Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,19 +7,36 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-  end
 
+    it "deberia tener el titulo 'Home'" do
+	visit '/static_pages/home'
+	expect(page).to have_title('Home')
+    end
+  end
+ 
    describe "Help page" do
       
       it "deberia de tener el contenido 'Help'" do
 	visit '/static_pages/help'
 	expect(page).to have_content('Help')
       end
+
+      it "deberia tener el titulo 'Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_title('Help')
+    end
+
    end
    describe "About page" do
       it "deberia de tener el contenido 'About Us'" do
 	visit '/static_pages/about'
 	expect(page).to have_content('About Us')
       end
+
+      it "deberia tener el titulo 'About'" do
+        visit '/static_pages/about'
+        expect(page).to have_title('About')
+    end
+
    end
 end
