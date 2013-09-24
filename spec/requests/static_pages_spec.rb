8bd72f3c459b440fee5ddee983254663b71c1c74
Xeada <xeada77@gmail.com>
2013-09-24 21:44:45 +0200

    Fin capitulo test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +13,7 @@ describe "StaticPages" do
 
     it "deberia tener el titulo 'Home'" do
 	visit '/static_pages/home'
-	expect(page).to have_title('Home')
+	expect(page).to have_title("#{base_title} | Home")
     end
   end
  
@@ -23,7 +26,7 @@ describe "StaticPages" do
 
       it "deberia tener el titulo 'Help'" do
         visit '/static_pages/help'
-        expect(page).to have_title('Help')
+        expect(page).to have_title("#{base_title} | Help")
     end
 
    end
@@ -35,8 +38,20 @@ describe "StaticPages" do
 
       it "deberia tener el titulo 'About'" do
         visit '/static_pages/about'
-        expect(page).to have_title('About')
+        expect(page).to have_title("#{base_title} | About")
     end
 
+   end
+
+   describe "Contact page" do
+      it "deberia de tener el contenido 'Contact'" do
+	visit '/static_pages/contact'
+	expect(page).to have_content('Contact')
+	end
+      
+      it "deberia tener el titulo 'Contact'" do
+	visit '/static_pages/contact'
+	expect(page).to have_title("#{base_title} | Contact")
+	end
    end
 end
